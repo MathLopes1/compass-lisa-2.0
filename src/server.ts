59bfbd717944b839487795935f89db044d69d65c
Matheus <matheus.pgm2000@gmail.com>
2022-03-21 19:58:06 -0300

    feat: initial project

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import  App  from './App';
+import { logger } from './app/utils/log/logger'
+
+App.listen( process.env.PORT, () => logger.info('application on port 3000'))
+
