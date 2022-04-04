c096e1cc4024f15cf3ed78abc7b179e5dc373ab6
Matheus <matheus.pgm2000@gmail.com>
2022-04-04 15:26:16 -0300

    feat: created router put cars

--- src/app/interfaces/Car/ICarRepository.ts
+++ src/app/interfaces/Car/ICarRepository.ts
@@ -4,4 +4,5 @@ export interface ICarRepository {
   create: (project: Icar) => Promise<Icar>
   find: () => Promise<Icar[]>
   findId: (id: string) => Promise<Icar>
+  updated: (id: string, payload: Icar) => Promise<Icar>
 }
