9d944475c233565fd37019c1ff9795fdc4b51c29
Matheus <matheus.pgm2000@gmail.com>
2022-04-07 22:43:18 -0300

    feat: created router getAll of peoples

--- src/app/interfaces/People/IPeopleRepository.ts
+++ src/app/interfaces/People/IPeopleRepository.ts
@@ -2,4 +2,5 @@ import { IPeople } from './IPeople';
 
 export interface IPeopleRepository {
     create: (payload: IPeople)=> Promise<IPeople>
+    find: () => Promise<IPeople[]>
 }
--- src/app/interfaces/People/IPeopleService.ts
+++ src/app/interfaces/People/IPeopleService.ts
@@ -1,5 +1,6 @@
 import { IPeople } from './IPeople';
 
 export interface IPeopleServices {
-    create: (payload: IPeople)=> Promise<IPeople>
+    create: (payload: IPeople) => Promise<IPeople>
+    find: () => Promise<IPeople[]>
 }
