bdac31018c76b9daef6db31a60afd60bce2622f7
Matheus <matheus.pgm2000@gmail.com>
2022-04-24 01:20:42 -0300

    feat: creating post rental route validation

---
+++ src/app/validations/RentalValidations/RentalBody.ts
@@ -0,0 +1,43 @@
+import Joi from 'joi';
+
+import { NextFunction, Response, Request } from 'express';
+import { Middleware } from '@decorators/express';
+import { IRental } from '../../interfaces/Rental/IRental';
+
+class validationBody implements Middleware {
+  async use(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
+    try {
+      const validation: Joi.ObjectSchema<IRental> = Joi.object({
+        nome: Joi.string().trim().min(3).max(20)
+          .required(),
+        cnpj: Joi.string().trim().min(14).max(14)
+          .required(),
+        atividades: Joi.string().min(5).max(40).trim()
+          .required(),
+        endereco: Joi.array().min(1).unique('cep')
+          .items(
+            Joi.object({
+              cep: Joi.string().min(8).max(8).trim()
+                .required(),
+              number: Joi.string().min(1).trim().required(),
+              complemento: Joi.string().min(5).max(40).trim(),
+              isFilial: Joi.boolean().required(),
+            }),
+          ),
+      });
+      const { error } = await validation.validate(req.body, { abortEarly: true });
+      if (error) throw error;
+
+      return next();
+    } catch (error) {
+      return res.status(400).json({
+        details: error.details.map((detail) => ({
+          name: detail.path[0],
+          description: detail.message,
+        })),
+      });
+    }
+  }
+}
+
+export default validationBody;
