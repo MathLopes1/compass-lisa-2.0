34c9ce471e1d5311859b14fb5ff5a52354cfbf6c
Matheus <matheus.pgm2000@gmail.com>
2022-04-24 17:09:40 -0300

    feat: starting car test

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+
+  collectCoverage: true,
+
+  coverageDirectory: 'coverage',
+
+  testMatch: [
+    '**/__tests__/*.spec.ts',
+  ],
+};
