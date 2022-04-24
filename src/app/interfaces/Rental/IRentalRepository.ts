7d71eed57817fa1f77be687f8d00dc7aed99a853
Matheus <matheus.pgm2000@gmail.com>
2022-04-23 21:25:31 -0300

    feat: creating get route by rental id

--- src/app/interfaces/Rental/IRentalRepository.ts
+++ src/app/interfaces/Rental/IRentalRepository.ts
@@ -3,4 +3,5 @@ import { IRental } from './IRental';
 export interface IRentalRepository {
     create: (payload: IRental) => Promise<IRental>
     find: (query) => Promise<IRental | IRental[]>
+    findId: (id: string) => Promise<IRental>
 }
